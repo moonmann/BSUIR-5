6be84b97e40d421968fa3bb92c9f301a83d19275
VasiliyKochurko <kochurkovasiliy@gmail.com>
2023-10-05 19:36:49 +0300

    Configured WebAPI and created BaseController

--- PBZ/Lab2/Application/DependencyInjection.cs
+++ PBZ/Lab2/Application/DependencyInjection.cs
@@ -8,7 +8,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection service)
     {
-        service.AddMediatR(Assembly.GetExecutingAssembly());
+        service.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         return service;
     }
 }
--- PBZ/Lab2/WebAPI/Program.cs
+++ PBZ/Lab2/WebAPI/Program.cs
@@ -1,6 +1,58 @@
+using System.Reflection;
+using Application;
+using Application.Common.Mappings;
+using Application.Interfaces;
+using Infrastructure;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(ILabDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<LabDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+}
+
+app.MapControllers();
 
 app.Run();
